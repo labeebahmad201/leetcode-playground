cf0ae7650b2e385dbaf5ae86ab392e2ed09c31c5
Labeeb Ahmad <labeebahmad201@gmail.com>
2024-02-18 09:56:08 -0500

    feat: 125 valid palindrom

---
+++ leetcode/125 valid palindrome.js
@@ -0,0 +1,40 @@
+function isAlphanumeric(char){
+    return ( 
+        (char.charCodeAt(0) >= 'a'.charCodeAt(0) && char.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
+        (char.charCodeAt(0) >= 'A'.charCodeAt(0) && char.charCodeAt(0) <= 'Z'.charCodeAt(0)) ||
+        (char.charCodeAt(0) >= '0'.charCodeAt(0) && char.charCodeAt(0) <= '9'.charCodeAt(0))
+    );
+}
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let l = 0;
+    let r = s.length - 1;
+
+    while(l < r){
+        while(l < s.length && !isAlphanumeric(s[l])){
+            l++
+        }
+
+        while(r >= 0 && !isAlphanumeric(s[r])){
+            r--;
+        }
+
+        if(l >= r){
+            break;
+        }
+
+        if(s[l].toLowerCase() != s[r].toLowerCase()){
+            return false;
+        }
+
+        l++;
+        r--;
+    }
+
+
+    return true;
+
+};
