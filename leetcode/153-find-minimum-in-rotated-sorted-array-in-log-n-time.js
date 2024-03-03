336ee5500bf1ccfe974e28cffdd7630475fc03c7
Labeeb Ahmad <labeebahmad201@gmail.com>
2024-03-02 23:28:45 -0500

    feat: update solution for LC 153

--- leetcode/153-find-minimum-in-rotated-sorted-array-in-log-n-time.js
+++ leetcode/153-find-minimum-in-rotated-sorted-array-in-log-n-time.js
@@ -3,36 +3,31 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let min = nums[0];
+    
+    let l = 0, r = nums.length - 1, min = nums[0];
+    while(l <= r){
 
-    if(nums.length == 1){
-        return min;
-    }
+        if(nums[l] < nums[r]){
+            min = Math.min(min, nums[l]);
+        }
 
-    let l = 0;
-    let r = nums.length - 1;
-
-    while(l < r){
-        let mid = parseInt((l + r) / 2);
+        const mid = Math.floor((l+r)/2);
         if(nums[mid] < min){
             min = nums[mid];
         }
-        if(nums[l] <= nums[mid]){
-            l = mid
-            // serch right
-        } else {
-            // search left
-            r = mid;
-        }
-        
-        if(l+1 == r){
-            min = nums[l] < min ? nums[l] : min;
-            min = nums[r] < min ? nums[r] : min;
-            break;             
-        }
-    }
 
-    return min;    
+        if(nums[l] <= nums[mid]){//isSortedArray
+            // search right cause min is there.
+            l = mid + 1;
+        } else { // if not left half then right is sorted
+            // search left cause min is there.
+            r = mid - 1;
+        }
+
+    }    
+
+
+    return min;
 };
 
 findMin('[3,4,5,1,2]'); // 1
