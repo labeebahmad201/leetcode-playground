f54506f35706c81202a82d4e7a39a22e1c214bb0
Labeeb Ahmad <labeebahmad201@gmail.com>
2024-03-04 22:00:54 -0500

    feat: 141 linked list cycle

---
+++ leetcode/141 linked list cycle.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head ? head.next : null;
+
+    if(slow === null && head === null){
+        return false;
+    }
+
+    while(fast && fast.next != null){
+        if(slow === fast){
+            return true;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return false;
+};
