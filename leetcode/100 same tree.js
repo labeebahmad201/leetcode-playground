62b75e939dbfcc26634907f91ce5a334ca8371b7
Labeeb <44992984+labeebahmad201@users.noreply.github.com>
2024-03-18 02:33:16 -0400

    feat: Update 100 same tree.js

--- leetcode/100 same tree.js
+++ leetcode/100 same tree.js
@@ -12,15 +12,13 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if(p == null && q == null){ 
+    if(p === null && q === null){
         return true;
-    } else if(p == null && q != null){
+    } else if(p === null || q == null){
         return false;
-    } else if(p != null && q == null){
-        return false;        
-    } else if(p?.val !== q?.val){
+    } else if(p.val != q.val){
         return false;
-    } else {
-        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right);
     }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
